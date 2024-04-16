091c0dbc27cfdaf93c4baa908b8812fccb30477e
luciomotta <83682095+luciomotta@users.noreply.github.com>
2024-04-15 21:02:57 -0300

    Styles
    Geral e por PAG

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React from 'react'; 
 import { View, Text, ScrollView, Image, StyleSheet } from 'react-native';
+import styles from '.src/stylesGeral.css';
 
 const Home = () => {
   return (
@@ -32,43 +33,5 @@ const Home = () => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    padding: 20,
-  },
-  banner: {
-    height: 200,
-    marginBottom: 20,
-  },
-  image: {
-    width: 300,
-    height: 200,
-    marginRight: 10,
-  },
-  cardContainer: {
-    marginBottom: 20,
-  },
-  row: {
-    flexDirection: 'row',
-    marginBottom: 10,
-  },
-  card: {
-    flex: 1,
-    backgroundColor: '#ccc',
-    marginRight: 10,
-    height: 100,
-  },
-  menu: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-  },
-  menuItem: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-});
 
 export default Home;
